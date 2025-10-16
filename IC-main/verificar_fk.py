c13c8c6317eb932ac5c65dd2729b22f46881b46c
Gabriel Freitas <gabrielfc051ic@gmail.com>
2025-10-16 11:11:32 -0300

    Sistema mais atualizado possível, com tudo funcionando corretamente

--- IC-main/verificar_fk.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("usuarios.db")
-cursor = conn.cursor()
-cursor.execute("PRAGMA foreign_key_list(process_activities);")
-for row in cursor.fetchall():
-    print(row)
-conn.close()
